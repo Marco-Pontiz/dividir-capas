1a42066d8b25a0340fa9bdd9ac8816452a38857f
Marco Pontiz <marcopontiz@gmail.com>
2023-02-24 17:06:55 -0300

    Fixing database

--- backend/src/persistance/db.connection.js
+++ backend/src/persistance/db.connection.js
@@ -7,12 +7,12 @@ const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 
 if (dbPassword) {
-    const mongoDB = `mongodb://127.0.0.1:27017/${dbName}` || 'mongodb://127.0.0.1:27017/testMongoDB';
+    const mongoDB = 'mongodb://127.0.0.1:27017/capas';
     mongoose.connect(mongoDB, {
         useNewUrlParser: true,
     });
     mongoose.connection.on('error', err => console.log(`DB connection ERR! ${err}`));
     mongoose.connection.once('open', () => console.log(`DB ${dbName} connected successfully at ${mongoDB}\n`));
 } else {
-    console.error(`DB connection ERR! Please enter your database password in .env file.`);
+    console.error(`DB connection ERR!`);
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use('/', apiRoutes);
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.listen(port,
     () => {
